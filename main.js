b4bca203df6eb679f0c64f84a7f8dc9b339375cd
marshmallowbox <tjdeh1230@naver.com>
2020-11-16 15:01:55 +0900

    20.11.16/15:01

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHTML = require('sanitize-html');
 var mysql = require("mysql");
-var msg = require ('dialog');
-var rawdata ;
+var msg = require('dialog');
+var rawdata;
 
 //RDS 접속 정보
 var connection = mysql.createConnection({
@@ -15,13 +15,13 @@ var connection = mysql.createConnection({
     password: "password",
     database: "data"
 });
-connection.connect(function(err){
-    if(err){
+connection.connect(function (err) {
+    if (err) {
         console.log(err);
-    } else{
-        connection.query(`SELECT * FROM information`, function(err, rows, fields){
+    } else {
+        connection.query(`SELECT * FROM information`, function (err, rows, fields) {
             rawdata = rows;
-            console.log("정상적으로 DB에 연결 되었습니다.\n"+rows.length+" 개의 데이터가 존재 합니다.")
+            console.log("정상적으로 DB에 연결 되었습니다.\n" + rows.length + " 개의 데이터가 존재 합니다.")
         });
     }
 });
@@ -39,25 +39,25 @@ var app = http.createServer(function (request, response) {
         // 쿼리데이터가 없을 때(초기 화면)
         if (queryData.id === undefined) {
             // refresh
-            connection.query(`SELECT * FROM information`, function(err, rows, fields){
+            connection.query(`SELECT * FROM information`, function (err, rows, fields) {
                 rawdata = rows;
-                console.log("정상적으로 DB에 접근 되었습니다.\n"+rows.length+" 개의 데이터가 존재 합니다.");
+                console.log("정상적으로 DB에 접근 되었습니다.\n" + rows.length + " 개의 데이터가 존재 합니다.");
             });
-            var title = "Welcome";
-            var description = "Hello, This is ddungdo's home";
+            var title = "&nbsp; Welcome";
+            var description = "&nbsp; Hello, This is Share Place home";
             var list = template.Listrds(rawdata);
             var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
-            `<table id="empList" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" data-upgraded=",MaterialDataTable">
+                `<table id="empList" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" data-upgraded=",MaterialDataTable">
             <tbody><tr id="yy" class="mdl-data-table__cell--non-numeric">
             <th><a href="/create">create</a></th>
-            </tr></table>`,"");
+            </tr></table>`, "");
             response.writeHead(200);
             response.end(html);
-        // 쿼리 데이터가 있을 때 (각 항목을 누른 상황)
+            // 쿼리 데이터가 있을 때 (각 항목을 누른 상황)
         } else {
             var filteredID = path.parse(queryData.id).base;
             // rds에서 데이터 받아오는 함수
-            connection.query("SELECT * FROM information WHERE title="+`"${filteredID}"`, function(err, rows, fields){
+            connection.query("SELECT * FROM information WHERE title=" + `"${filteredID}"`, function (err, rows, fields) {
                 console.log(rows[0].title);
                 var title = rows[0].title;
                 var data = rows[0].description;
@@ -65,7 +65,7 @@ var app = http.createServer(function (request, response) {
                 var longtitude = rows[0].longtitude;
                 var list = template.Listrds(rawdata);
                 var html = template.HTML(title, list,
-                    `<div id="tt"><h2 class="tt">${title}</h2>
+                `<div id="tt"><h2 class="tt">${title}</h2>
                   <div class="grid__container">
                      <div class="form--login" id="ds">
                         <br>
@@ -95,7 +95,7 @@ var app = http.createServer(function (request, response) {
                         <input type="submit" class="del" value="delete">
                     </form></th>
                 </tr></table>
-                    `,`
+                    `, `
                     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=645c80425f05e9d578b464a870031381"></script>
                     <div id="map" style="width:100%;height:350px;"></div>
                     <div id="clickLatlng"></div>
@@ -141,8 +141,8 @@ var app = http.createServer(function (request, response) {
                 response.end(html);
             });
         }
-    // create 항목을 눌렀을 때
-    } else if(pathname ==='/create'){
+        // create 항목을 눌렀을 때
+    } else if (pathname === '/create') {
         var title = 'WEB - create';
         var list = template.Listrds(rawdata);
         var latitude = "37.498284";
@@ -176,13 +176,13 @@ var app = http.createServer(function (request, response) {
             </form>
             
             
-            <p><em>지도를 클릭해주세요!</em></p> 
 
-            `,"",`
+            `, "", `
             
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=645c80425f05e9d578b464a870031381"></script>
-            <div id="map" style="width:100%;height:350px;"></div>
-            <p><em>지도를 클릭해주세요!</em></p> 
+            <div class="grid">
+            <div id="map" style="width:100%;height:400px;"></div>
+            </div>
             <script>
             var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
                 mapOption = { 
@@ -230,55 +230,55 @@ var app = http.createServer(function (request, response) {
         response.writeHead(200);
         response.end(html);
 
-    // create 항목에서 제출을 클릭했을 때 302를 반환하고 파일을 생성
-    } else if(pathname ==='/create_process'){
+        // create 항목에서 제출을 클릭했을 때 302를 반환하고 파일을 생성
+    } else if (pathname === '/create_process') {
         var body = '';
-        request.on('data', function(data){
+        request.on('data', function (data) {
             body = body + data;
         });
-        request.on('end', function(){
+        request.on('end', function () {
             var post = qs.parse(body);
             console.log(post);
-            if(post.form_name===""){
+            if (post.form_name === "") {
                 msg.err("지도를 클릭 후 저장해주세요~");
-                response.writeHead(302, {Location: `/create`});
+                response.writeHead(302, { Location: `/create` });
                 response.end('success');
                 return 0;
             }
             var title = sanitizeHTML(post.title);
             var description = sanitizeHTML(post.description);
-            console.log("title : "+ title);
-            console.log("description : "+ description);
-            connection.query(`INSERT INTO information VALUES("${title}", "${post.form_name}", "${post.form_name2}", "${description.replace(/\r\n/g,"<br>")}")`, function(err, rows, fields){
+            console.log("title : " + title);
+            console.log("description : " + description);
+            connection.query(`INSERT INTO information VALUES("${title}", "${post.form_name}", "${post.form_name2}", "${description.replace(/\r\n/g, "<br>")}")`, function (err, rows, fields) {
                 // 오류가 발생 할 경우 에러 메세지 표시
-                if(err){
+                if (err) {
                     console.log(err.sqlMessage);
                     msg.err(err.sqlMessage);
-                    response.writeHead(302, {Location: `/`});
+                    response.writeHead(302, { Location: `/` });
                     response.end('success');
-                }else{
-                    connection.query(`SELECT * FROM information`, function(err, rows, fields){
+                } else {
+                    connection.query(`SELECT * FROM information`, function (err, rows, fields) {
                         rawdata = rows;
-                        console.log("정상적으로 DB에 저장 되었습니다.\n"+rows.length+" 개의 데이터가 존재 합니다.");
-                        response.writeHead(302, {Location: `/?id=${qs.escape(title)}`});
+                        console.log("정상적으로 DB에 저장 되었습니다.\n" + rows.length + " 개의 데이터가 존재 합니다.");
+                        response.writeHead(302, { Location: `/?id=${qs.escape(title)}` });
                         response.end('success');
                     });
                 }
             });
         });
-    // 각 항목을 누른 후 update를 눌렀을 때 queryData.id 를 받아와서 해당 내용을 표출 및 update
-    } else if(pathname ==='/update'){
+        // 각 항목을 누른 후 update를 눌렀을 때 queryData.id 를 받아와서 해당 내용을 표출 및 update
+    } else if (pathname === '/update') {
         var filteredID = path.parse(queryData.id).base;
-            // rds에서 데이터 받아오는 함수
-            connection.query("SELECT * FROM information WHERE title="+`"${filteredID}"`, function(err, rows, fields){
-                console.log(rows[0].title);
-                var title = rows[0].title;
-                var data = rows[0].description;
-                var latitude = parseFloat(rows[0].latitude);
-                var longtitude = parseFloat(rows[0].longtitude);
-                console.log(rows[0]);
-                var list = template.Listrds(rawdata);
-                var html = template.HTML(title, list, `
+        // rds에서 데이터 받아오는 함수
+        connection.query("SELECT * FROM information WHERE title=" + `"${filteredID}"`, function (err, rows, fields) {
+            console.log(rows[0].title);
+            var title = rows[0].title;
+            var data = rows[0].description;
+            var latitude = parseFloat(rows[0].latitude);
+            var longtitude = parseFloat(rows[0].longtitude);
+            console.log(rows[0]);
+            var list = template.Listrds(rawdata);
+            var html = template.HTML(title, list, `
             <form action="/update_process" method="post" class="form form--login" name="createtag">
             <input type="hidden" name="form_name" value="${latitude}">
             <input type="hidden" name="form_name2" value="${longtitude}">
@@ -298,23 +298,23 @@ var app = http.createServer(function (request, response) {
                   <label class="fontawesome-user" for="name"> 경도</label>
                   <label class="st" id="clickLatlng2">${longtitude}</label>
                   <div id="clickLatlng2"></div>
-               </div><br><br><br>
-               <div class="form__filed"><br><br>
+               </div><br><br>
+               <div class="form__filed"><br>
                     <label class="fontawesome-user" for="name"> 내용</label></br>
-                    <textarea name="description" placeholder="설명" required>${data.replace(/<br>/g,"\r\n")}</textarea>
+                    <textarea name="description" placeholder="설명" required>${data.replace(/<br>/g, "\r\n")}</textarea>
                </div>
                <input type="submit">
             </div>
             </form>
             
             
-            <p><em>지도를 클릭해주세요!</em></p> 
 
-            `,"",`
+            `, "", `
             
             <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=645c80425f05e9d578b464a870031381"></script>
-            <div id="map" style="width:100%;height:350px;"></div>
-            <p><em>지도를 클릭해주세요!</em></p> 
+            <div class="grid">
+            <div id="map" style="width:100%;height:400px;"></div>
+            </div>
             <script>
             var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
                 mapOption = { 
@@ -359,70 +359,70 @@ var app = http.createServer(function (request, response) {
             });
             </script>
             `);
-                response.writeHead(200);
-                response.end(html);
-            });
-    // update_process 부분
-    }else if(pathname ==="/update_process"){
+            response.writeHead(200);
+            response.end(html);
+        });
+        // update_process 부분
+    } else if (pathname === "/update_process") {
         // 데이터 나누어서 받고 (데이터가 너무 클 경우에는 exception 처리 해주는 함수 추가 필요)
         var body = '';
-        request.on('data', function(data){
+        request.on('data', function (data) {
             body = body + data;
         });
         // 데이터가 모두 
-        request.on('end', function(){
+        request.on('end', function () {
             var post = qs.parse(body);
             console.log(post);
             // 현재 적혀있는 title, latitude, longtitude, description을 저장 => 원래 있던 title로 구분 (primary key 여서 가능)
             var title = sanitizeHTML(post.title);
             var description = sanitizeHTML(post.description);
-            console.log("id : "+ post.id);
-            console.log("title : "+ title);
-            console.log("description : "+ description);
-            connection.query(`UPDATE information SET title = "${title}", latitude = "${post.form_name}", longtitude = "${post.form_name2}", description = "${description.replace(/\r\n/g,"<br>")}" WHERE title = "${post.id}"`, function(err, rows, fields){
+            console.log("id : " + post.id);
+            console.log("title : " + title);
+            console.log("description : " + description);
+            connection.query(`UPDATE information SET title = "${title}", latitude = "${post.form_name}", longtitude = "${post.form_name2}", description = "${description.replace(/\r\n/g, "<br>")}" WHERE title = "${post.id}"`, function (err, rows, fields) {
                 // error 
-                if(err){
+                if (err) {
                     console.log(err.sqlMessage);
                     msg.err(err.sqlMessage);
-                    response.writeHead(302, {Location: `/`});
+                    response.writeHead(302, { Location: `/` });
                     response.end('success');
-                }else{
-                    connection.query(`SELECT * FROM information`, function(err, rows, fields){
+                } else {
+                    connection.query(`SELECT * FROM information`, function (err, rows, fields) {
                         rawdata = rows;
-                        console.log("정상적으로 DB에 저장 되었습니다.\n"+rows.length+" 개의 데이터가 존재 합니다.");
+                        console.log("정상적으로 DB에 저장 되었습니다.\n" + rows.length + " 개의 데이터가 존재 합니다.");
                     });
-                    response.writeHead(302, {Location: `/?id=${qs.escape(title)}`});
+                    response.writeHead(302, { Location: `/?id=${qs.escape(title)}` });
                     response.end('success');
                 }
             });
         });
-    } 
+    }
     // 삭제 기능 
-    else if(pathname ==="/delete_process"){
+    else if (pathname === "/delete_process") {
         var body = '';
-        request.on('data', function(data){
+        request.on('data', function (data) {
             body = body + data;
         });
-        request.on('end', function(){
+        request.on('end', function () {
             // 넘긴 값을 parse로 받아주는 형식
             var post = qs.parse(body);
             var id = post.id;
             var filteredID = path.parse(id).base;
-            connection.query(`DELETE from information where title = "${filteredID}"`, function(err, rows, fields){
-                if (err){
+            connection.query(`DELETE from information where title = "${filteredID}"`, function (err, rows, fields) {
+                if (err) {
                     console.log(err.sqlMessage);
                     msg.err(err.sqlMessage);
-                    response.writeHead(302, {Location: `/`});
+                    response.writeHead(302, { Location: `/` });
                     response.end('success');
-                }else{
-                    connection.query(`SELECT * FROM information`, function(err, rows, fields){
+                } else {
+                    connection.query(`SELECT * FROM information`, function (err, rows, fields) {
                         rawdata = rows;
-                        console.log("정상적으로 DB에서 삭제 되었습니다.\n"+rows.length+" 개의 데이터가 존재 합니다.");
+                        console.log("정상적으로 DB에서 삭제 되었습니다.\n" + rows.length + " 개의 데이터가 존재 합니다.");
                     });
-                    response.writeHead(302, {Location: `/`});
+                    response.writeHead(302, { Location: `/` });
                     response.end('success');
                 }
-                
+
             });
         });
     }
